af1ed3859f6e8e6c9b6c11f290900d02ee143e22
Vesselin Vassilev <vesselin.vasilev@gmail.com>
2020-05-30 12:41:38 +1000

    add scheduled tasks monitor widget

---
+++ Mvc/Controllers/ScheduledTasksMonitorController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+using Telerik.Sitefinity.Mvc.ActionFilters;
+using Telerik.Sitefinity.Scheduling;
+using Telerik.Sitefinity.Scheduling.Model;
+using Telerik.Sitefinity.Data;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "ScheduledTasksMonitor", Title = "Tasks Monitor", SectionName = "Admin Widgets", CssClass = "sfMvcIcn")]
+    public class ScheduledTasksMonitorController : Controller
+    {
+        public string FilterExpression { get; set; }
+
+        public string OrderExpression { get; set; } = "ExecuteTime ASC";
+
+        public int RefreshIntervalMs { get; set; } = 5000;
+
+        [HttpGet]
+        public ActionResult Index()
+        {            
+            ViewBag.RefreshIntervalMs = RefreshIntervalMs;
+
+            return View("TaskMonitor");
+        }
+
+        [HttpGet]
+        [StandaloneResponseFilter]
+        public ActionResult Progress()
+        {
+            List<ScheduledTaskData> tasks = GetScheduledTasks();
+
+            return Json(tasks, JsonRequestBehavior.AllowGet);
+        }
+
+        private List<ScheduledTaskData> GetScheduledTasks()
+        {
+            var manager = SchedulingManager.GetManager();
+
+            // we don't need total count
+            int? count = null;
+
+            var tasks = DataProviderBase.SetExpressions(manager.GetTaskData(),
+                                                        FilterExpression,
+                                                        OrderExpression,
+                                                        null,
+                                                        null,
+                                                        ref count);
+                               
+            return tasks.ToList();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Sitefinity MVC (Feather) widgets.
 
 **List of widgets**
 
-- **Scheduled Tasks Monitor** - widget that provides UI to see and manage all Sitefinity scheduled tasks. The widget can be dropped on a custom backend page and allow the backend users to see a list of scheduled tasks. Failed or Stopped tasks can be retried or deleted.
+- **Scheduled Tasks Monitor** - widget that provides UI to see and manage all Sitefinity scheduled tasks. The widget can be dropped on a custom backend page and allow the backend users to see a list of scheduled tasks. Stopped tasks can be deleted. Failed tasks can be either retried or deleted.
 
